a192af3c7d9e2279e888391943ca3fa38a9de10d
Guneet Rekhi <rekhi.guneet@gmail.com>
2022-10-07 13:09:06 -0700

    added AddContact comp. & configured css for header

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-//import AddContact from './components/AddContact';
+import AddContact from './components/AddContact';
 //import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div>
       <Header />
-      {/*<AddContact />
-      <ContactList />*/}
+      <AddContact />
+      {/*<ContactList />*/}
     </div>
   );
 }
