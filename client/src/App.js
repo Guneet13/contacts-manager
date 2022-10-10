e46c7f3c31724748f7478098611a6584f6f4fced
Guneet Rekhi <rekhi.guneet@gmail.com>
2022-10-10 14:14:50 -0700

    rendering a dummy list of contacts

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import AddContact from './components/AddContact';
-//import ContactList from './components/ContactList';
+import ContactList from './components/ContactList';
 
 function App() {
+
+  //passing data to another component(rendering list)
+  const contacts = [
+    {
+      id: "1",
+      "name": "Olivia R",
+      "email": "or@gmail.com",
+    },
+    {
+      id: "2",
+      "name": "Sophia M",
+      "email": "sm@gmail.com",
+    }
+  ];
   return (
-    <div>
+    <div className='ui container'>
       <Header />
       <AddContact />
-      {/*<ContactList />*/}
+      <ContactList contacts={contacts}/>
     </div>
   );
 }
